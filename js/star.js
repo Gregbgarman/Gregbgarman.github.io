47f088cd44c4b3b5f9c93b74f8746bcade9232b0
Greg <gregory.b.garman@gmail.com>
2022-10-19 15:12:54 -0400

    adding js folder

---
+++ js/star.js
@@ -0,0 +1,8 @@
+
+document.getElementById("printStarDemoReceipt").addEventListener("click", printStarDemoReceipt);
+
+function printStarDemoReceipt(){
+
+   document.getElementById("textfield").value="clicked";
+
+}
