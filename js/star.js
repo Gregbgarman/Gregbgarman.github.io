350056ccf9f70b56a0c14cb9d370e08cd2950cf4
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2022-10-24 15:55:44 -0400

    Update star.js

--- js/star.js
+++ js/star.js
@@ -1,8 +1,17 @@
 
 document.getElementById("PrintStarDemoReceipt").addEventListener("click", PrintStarDemoReceipt);
+document.getElementById("SearchStarPrinters").addEventListener("click", searchStarPrinters);
+document.getElementById("ConnectPrinter").addEventListener("click", ConnectPrinter);
+
 
 function PrintStarDemoReceipt(){
-
    document.getElementById("TextField").value=EloStarPrinterManager.printStarDemoReceipt();
+}
 
+function ConnectPrinter(){
+   document.getElementById("TextField").value=EloStarPrinterManager.ConnectPrinter();
+}
+
+function searchStarPrinters(){
+   document.getElementById("TextField").value=EloStarPrinterManager.searchStarPrinters("LAN:");
 }
