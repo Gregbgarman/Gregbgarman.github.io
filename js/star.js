e8f000608999ff45250e1073953f9c45a19af911
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2022-10-27 13:19:33 -0400

    Update star.js

--- js/star.js
+++ js/star.js
@@ -7,6 +7,9 @@ document.getElementById("searchPrinter").addEventListener("click", searchPrinter
 
 function getPort(){
    var target=document.getElementById("TextField").value;
+   if(target.length > 1 && target.charAt(0) == '[' && target.charAt(target.length-1) == ']') {
+        target = target.slice(1, -1).split(',')[0];
+    }
    document.getElementById("TextField").value=EloStarPrinterManager.getPort(target,"",10000);
 }
 
