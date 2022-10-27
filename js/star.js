041de79f632e5d3aba6363dabc1e8dddcfbbea8b
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2022-10-27 12:36:31 -0400

    Update star.js

--- js/star.js
+++ js/star.js
@@ -1,7 +1,21 @@
 
 document.getElementById("PrintStarDemoReceipt").addEventListener("click", PrintStarDemoReceipt);
-document.getElementById("SearchStarPrinters").addEventListener("click", searchStarPrinters);
 document.getElementById("ConnectPrinter").addEventListener("click", ConnectPrinter);
+
+
+document.getElementById("getPort").addEventListener("click", getPort);
+document.getElementById("SearchPrinter").addEventListener("click", searchPrinter);
+function getPort(){
+   document.getElementById("TextField").value=EloStarPrinterManager.getPort();
+}
+
+
+function searchPrinter(){
+   document.getElementById("TextField").value=EloStarPrinterManager.searchPrinter("USB:");
+}
+
+
+
 
 
 function PrintStarDemoReceipt(){
@@ -12,6 +26,4 @@ function ConnectPrinter(){
    document.getElementById("TextField").value=EloStarPrinterManager.ConnectPrinter();
 }
 
-function searchStarPrinters(){
-   document.getElementById("TextField").value=EloStarPrinterManager.searchStarPrinters("USB:");
-}
+
