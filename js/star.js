b32dc5f2dde8cdd3399ecd48760048f61a33f733
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2022-11-01 17:26:05 -0400

    Update star.js

--- js/star.js
+++ js/star.js
@@ -211,8 +211,7 @@ function PrintStarDemoReceipt(){
          return;
    }
    
-   var Commands =""; 
-   EloStarPrinterManager.getCommands();
+   var Commands = EloStarPrinterManager.getCommands();
    document.getElementById("SuccessField").value=typeof Commands;
    return;
    
