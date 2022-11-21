f1909c7b44b7b5fe9a4c93e6ecfa83ac655feef8
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2022-11-21 13:43:56 -0500

    Update star.js

--- js/star.js
+++ js/star.js
@@ -264,6 +264,12 @@ function getReceipt1Data(){
         EloStarPrinterManager.appendAlignment("Center")
         EloStarPrinterManager.append("Guest No 1\n")
         EloStarPrinterManager.appendAlignment("Left")
+   
+        EloStarPrinterManager.appendHorizontalTabPosition([26])
+        EloStarPrinterManager.append("1 ice cream sundae\t4.50\n");
+
+   
+   
         EloStarPrinterManager.append("1 ice cream sundae             4.50\n1 soda pop                     1.75\n1 french fries                 4.00\n");
         EloStarPrinterManager.appendAlignment("Center");
         EloStarPrinterManager.append("Guest No 2\n")
