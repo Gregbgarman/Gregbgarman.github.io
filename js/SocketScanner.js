201d5c8ea18308c422df0e80dfe9e731cfb35ea1
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2023-01-23 20:35:54 -0500

    Update SocketScanner.js

--- js/SocketScanner.js
+++ js/SocketScanner.js
@@ -2,21 +2,20 @@ document.getElementById("initScanner").addEventListener("click", initScanner)
 document.getElementById("initSocketCam").addEventListener("click", initSocketCam)
 document.getElementById("runScanner").addEventListener("click", runScanner)
 document.getElementById("readData").addEventListener("click", readData)
-document.getElementById("StarPrinterAvailable").innerHTML = "Star Printer Disconnected"
 
 
 function initScanner(){
-    EloSocketMobileManager.initScanner();
+    EloSocketMobileManager.initScanner()
 }
 
 function initSocketCam(){
-  EloSocketMobileManager.initSocketCam();
+  EloSocketMobileManager.initSocketCam()
 }
 
 function runScanner(){
-  EloSocketMobileManager.runScanner();
+  EloSocketMobileManager.runScanner()
 }
 
 function readData(){
-  document.getElementById("textField").value = EloSocketMobileManager.readData();
+  document.getElementById("textField").value = EloSocketMobileManager.readData()
 }
