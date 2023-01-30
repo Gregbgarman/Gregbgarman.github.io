170fe20cbf9902be20baf16013563f84561757fc
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2023-01-30 16:02:06 -0500

    Update SocketScanner.js

--- js/SocketScanner.js
+++ js/SocketScanner.js
@@ -2,7 +2,13 @@ document.getElementById("initScanner").addEventListener("click", initScanner)
 document.getElementById("initSocketCam").addEventListener("click", initSocketCam)
 document.getElementById("runScanner").addEventListener("click", runScanner)
 document.getElementById("readData").addEventListener("click", readData)
+document.getElementById("readData").addEventListener("click", startService)
 
+
+
+function startService(){
+     EloSocketMobileManager.startService()
+}
 
 function initScanner(){
     EloSocketMobileManager.initScanner()
