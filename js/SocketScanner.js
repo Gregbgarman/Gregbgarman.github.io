7b00d08f9861cd330741476be8fcf9d5e30f903f
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2023-02-06 22:00:17 -0500

    Update SocketScanner.js

--- js/SocketScanner.js
+++ js/SocketScanner.js
@@ -6,9 +6,9 @@ document.getElementById("enableScanning").addEventListener("click", enableScanni
 document.getElementById("runScanner").addEventListener("click", runScanner)
 document.getElementById("disableScanning").addEventListener("click", disableScanning)
 
-//document.getElementById("initSocketCam").addEventListener("click", initSocketCam)
-//document.getElementById("activateSocketCam").addEventListener("click", activateSocketCam)
-//document.getElementById("disableSocketCam").addEventListener("click", disableSocketCam)
+document.getElementById("initSocketCam").addEventListener("click", initSocketCam)
+document.getElementById("activateSocketCam").addEventListener("click", activateSocketCam)
+document.getElementById("disableSocketCam").addEventListener("click", disableSocketCam)
 
 
 
