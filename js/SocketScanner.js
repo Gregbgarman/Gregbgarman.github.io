bfdc8a4b389bac12fd6d91ba70fbcaf227e8749a
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2023-02-07 11:40:35 -0500

    Update SocketScanner.js

--- js/SocketScanner.js
+++ js/SocketScanner.js
@@ -116,5 +116,4 @@ function getFoundBluetoothDevice(Data){     //use this function in development
          BluetoothDevices.push(Data)
          document.getElementById("textField").value = BluetoothDevices.toString()
      }
-    document.getElementById("textField").value = "sauce"
 }
