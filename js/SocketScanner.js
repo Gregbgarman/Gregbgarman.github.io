2558eb0485154e485096496120a2aec64b2bd42e
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2023-02-11 13:11:55 -0500

    Update SocketScanner.js

--- js/SocketScanner.js
+++ js/SocketScanner.js
@@ -4,14 +4,29 @@ This is a sample website containing code examples of how a Socket Mobile handhel
 **A prerequisite of using a Socket Mobile handheld scanner is that the Socket Mobile Companion app must be installed on the Elo device.
 It can be found here on the Google Play Store https://play.google.com/store/apps/details?id=com.socketmobile.companion&hl=en_US&gl=US
 
-However, this app will never have to be opened manually. Its purpose is to manage a service required for the scanner to connect to an 
-Android device over bluetooth. That service will be started by pressing the "Init Capture Service" on this sample website.
+However, the Companion app will never have to be opened manually. The Companion app will manage a service required for the scanner to hold 
+a connection to an Android device over bluetooth. That service will be started by pressing the "Init Capture Service" on this sample website.
 
-Once the mentioned service is running, the handheld scanner may connecct to the Elo device over bluetooth. The process involves first 
+Once the mentioned service is running, the next step is to connect the handheld scanner to the Elo device over bluetooth. The process involves first 
 scanning a particular QR code to make the Socket Mobile device discoverable, then it can be connected to. Pressing buttons 2-4, in order,
-on this website achieves this goal.
+on this website achieves this goal of connecting to the device over bluetooth.
 
 After the scanner is connected to the Elo device, scanning may be enabled, and barcodes can then be read.
+
+There are two components to differentiate and they are:
+1. The capture client
+2. The actual handheld scanner
+
+The capture client is responsible for monitoring the handheld scanner's device state and data received after a scan. It must first be initialized 
+properly for the handheld device to be used, and when the capture client is disconnected from, scanning will no longer be possible. This website is setup 
+such that there are several callbacks that will run when -
+A. The handheld scanner's device state has changed. 
+   - 4 possible states - GONE, AVAILABLE, OPEN, and READY. We connect to handheld scanner in the AVAILABLE state, and READY state should then follow.
+B. When an item has been scanned (Barcode, Qr code etc.)
+C. When the capture client's connection state has changed.
+   - 4 possible states - CONNECTING, CONNECTED, DISCONNECTING, DISCONNECTED
+
+
 */
 
 
@@ -30,11 +45,12 @@ let BluetoothDevices = []
 
 document.getElementById("scannerAvailable").innerHTML = "Scanner Unavailable"
 
-function initService(){
+                         
+function initService(){   //step 1. Start the capture service - Will run the Companion app in the background. You don't need to open it.
      document.getElementById("textField").value = EloSocketMobileManager.initService()
 }
 
-function showScanCode(){
+function showScanCode(){           //step 2. Reveal a special Qr code on this website that when scanned, enables the scanner to be found over BT
      if (!PairCodeShown){                   
          document.getElementById("appmodeqrcode").style.visibility = 'visible'
          document.getElementById("showScanCode").innerHTML="Hide Pair Code"
@@ -47,12 +63,14 @@ function showScanCode(){
      }
 }
 
-function searchBluetooth(){
-    registerBTSearchListener()                                        //see function below
+function searchBluetooth(){        //step 3. Search for the scanner over bluetooth. This involves setting a listener to receive the device 
+                                   //as soon as it is found. Once it is retrieved, it can be connected to.
+    registerBTSearchListener()                                        
     document.getElementById("textField").value = "searching..."
     BluetoothDevices = []
-    let BTAddress = "60:8A:10:64:A9:68"       //Find this on the handheld scanner to improve bluetooth pairing. If can't find, pass blank string "" as parameter     
-    let success = EloSocketMobileManager.searchBluetooth("")         //will find devices with "Socket" in their name when pass a blank string
+    //let BTAddress = "60:8A:10:64:A9:68"                //Find this address on the handheld scanner to improve bluetooth pairing. If can't find, pass blank string "" as parameter
+    let BTAddress = ""
+    let success = EloSocketMobileManager.searchBluetooth(BTAddress)         //will find devices with "Socket" in their name when pass a blank string
     if(success){
         document.getElementById("textField").value = "searching..."
     }
@@ -63,10 +81,10 @@ function searchBluetooth(){
 
                                        //setting BT search listener to receive discovered device(s) as soon as they are found
 function registerBTSearchListener(){
-    EloSocketMobileManager.registerBTSearchListener("BTSearchCallback")
+    EloSocketMobileManager.registerBTSearchListener("BTSearchCallback")    //this name needs to match the name of the function that receives data. See below.
 }
 
-function BTSearchCallback(Data){      //runs when bluetooth device is found. Will return " " if no device found at end of BT search
+function BTSearchCallback(Data){      //receives data in real time when bluetooth device is found. Will return " " if no device found at end of BT search
      let BTDeviceFound = Data
      if (BTDeviceFound === " "){
           document.getElementById("textField").value = "No device found"        
@@ -78,8 +96,8 @@ function BTSearchCallback(Data){      //runs when bluetooth device is found. Wil
 }
 
 function connectBluetooth(){
-    let BTDevice = document.getElementById("textField").value
-    let success =  EloSocketMobileManager.connectBluetooth(BTDevice)
+    let BTDevice = document.getElementById("textField").value              //Reading the BT device name found after searching bluetooth
+    let success =  EloSocketMobileManager.connectBluetooth(BTDevice)       //Passing BT name to connect to the device
     if (success){
         document.getElementById("textField").value = "Connecting...Wait for beep" 
     }
@@ -88,8 +106,12 @@ function connectBluetooth(){
     }
 }
 
+/////////////////////////////////////////// 
+//    Begin functions dealing with handheld scanner functionality
+///////////////////////////////////////////
+
 function enableScanning(){   
-    let success1 = EloSocketMobileManager.setClientListener("DeviceStateCallback")
+    let success1 = EloSocketMobileManager.setClientListener("DeviceStateCallback")        //set listener to receive device state changes (see below) 
     let success2 = EloSocketMobileManager.connectClient()
     if (success1 && success2){
        document.getElementById("textField").value =  "Waiting for device" 
@@ -99,9 +121,9 @@ function enableScanning(){
     }
 }
 
-function DeviceStateCallback(Data){
+function DeviceStateCallback(Data){          //receives events related to device state in real time. 4 possible states.
     let DeviceState = Data
-    if (DeviceState === "DEVICESTATE_GONE"){
+    if (DeviceState === "DEVICESTATE_GONE"){      
         // Scanner is gone
         document.getElementById("scannerAvailable").innerHTML = "Scanner Unavailable"
         document.getElementById("textField").value =  ""
@@ -121,6 +143,16 @@ function DeviceStateCallback(Data){
          document.getElementById("textField").value =  ""
     }   
 }
+
+////*****////
+function registerScanningListener(){                                  //setting listener to receive scanned data in real time.
+    EloSocketMobileManager.registerScanningListener("ScanDataCallback")     
+}
+
+function ScanDataCallback(Data){                           //when a barcode is scanned, its output will be received here.
+     document.getElementById("textField").value = Data
+}
+////****////
 
 
 function runScanner(){
@@ -147,14 +179,6 @@ function disableScanning(){
 }
 
 
-////*****////
-function registerScanningListener(){
-    EloSocketMobileManager.registerScanningListener("ScanDataCallback")     //parameter needs to match the function name below  
-}
 
-function ScanDataCallback(Data){       
-     document.getElementById("textField").value = Data
-}
-////****////
 
 
