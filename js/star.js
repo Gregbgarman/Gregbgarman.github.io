5d0fff4b1747e1f4b37b07c40f998ded5404db79
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2023-04-08 17:24:49 -0400

    Update star.js

--- js/star.js
+++ js/star.js
@@ -494,7 +494,7 @@ function BTHide(){
 function changePrinterBTName(){         //API's used in this function will be available in MR-28 and Android 12
     
     let emulation = "StarPRNT"
-    let name = "mC-Print3-D0100"        
+    let name = "mC-Print3-greg"        
     
     let BTManager_Key =  EloStarPrinterManager.getBTManager(PrinterPortName,"",10000, emulation)
     if (BTManager_Key === ''){
@@ -503,9 +503,11 @@ function changePrinterBTName(){         //API's used in this function will be av
     
     EloStarPrinterManager.openBTPort(BTManager_Key)
     EloStarPrinterManager.loadBTSetting(BTManager_Key)
-    if (EloStarPrinterManager.getBTDeviceNameCapability(BTManager_Key) === "SUPPORT"){       
-        EloStarPrinterManager.setBTDeviceName(BTManager_Key, name)       
-        EloStarPrinterManager.applyBTSetting(BTManager_Key)            
+    if (EloStarPrinterManager.getBTDeviceNameCapability(BTManager_Key) === "SUPPORT"){
+        if (EloStarPrinterManager.getBTDeviceName !== name){
+            EloStarPrinterManager.setBTDeviceName(BTManager_Key, name)       
+            EloStarPrinterManager.applyBTSetting(BTManager_Key)
+        }                   
     }
      if (EloStarPrinterManager.isBTPortOpened(BTManager_Key) === 1){
             EloStarPrinterManager.closeBTPort(BTManager_Key)
