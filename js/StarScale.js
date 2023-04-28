e6dfe8edc6809711e78a2efc741f1d4209c1a321
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2023-04-28 12:57:35 -0400

    Update StarScale.js

--- js/StarScale.js
+++ js/StarScale.js
@@ -1,6 +1,18 @@
-const json = '{"result":true, "count":42}'
+document.getElementById("beginScan").addEventListener("click", beginScan)
+document.getElementById("connectScale").addEventListener("click", connectScale)
+
+
+
+
+function beginScan(){
+document.getElementById("textField").value = 'hello'  
+}
+
+function connectScale(){
+  const json = '{"result":true, "count":42}'
 const obj = JSON.parse(json)
 
 
 
 document.getElementById("textField").value = obj.count
+}
