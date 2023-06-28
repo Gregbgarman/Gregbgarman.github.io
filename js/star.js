bccdb9fb147ca0f99dc1c811a468ce305a2c7d3a
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2023-06-28 17:58:05 -0400

    Update star.js

--- js/star.js
+++ js/star.js
@@ -55,7 +55,6 @@ let PrinterPortName=""        //Acquired from searching for printers and used ve
 let EmulationTable = {}
 initEmulationTable()
 
-EloStarPrinterManager.scanForScales("DeviceCallback", "All")
 
 function queryStarPrinterList(){             //enter either USB, BT, or TCP in test field box. Or leave blank to search USB -> BT -> TCP on this sample app
     let target=document.getElementById("textField").value
