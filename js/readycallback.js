bed22a6e57d9ffe6b9fe2b24cd46dcb67b04b73d
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2023-08-14 12:01:31 -0400

    Create readycallback.js

---
+++ js/readycallback.js
@@ -0,0 +1,9 @@
+/*
+document.getElementById("beginScan").addEventListener("click", beginScan)
+document.getElementById("connectScale").addEventListener("click", connectScale)
+document.getElementById("disconnectScale").addEventListener("click", disconnectScale)
+document.getElementById("setContOutput").addEventListener("click", setContinousOutput)
+document.getElementById("setStableOutput").addEventListener("click", setStableOutput)
+document.getElementById("getDeviceInfo").addEventListener("click", getDeviceInfo)
+document.getElementById("seeMoreData").addEventListener("click", seeMoreData)
+*/
