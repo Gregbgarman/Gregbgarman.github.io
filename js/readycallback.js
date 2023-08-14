1c4d2ce9987eced865c5dcbedb007848c8907643
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2023-08-14 14:30:33 -0400

    Update readycallback.js

--- js/readycallback.js
+++ js/readycallback.js
@@ -1,7 +1,19 @@
+let peripheralmanagerReady = false
+let honeywellscannerReady = false
+let zebrascannerReady = false
+let handheldscannerReady = false
+let epsonprinterReady = false
+let socketscannerReady = false
+let starprinterReady = false
+let starscaleReady = false
+
 
 
 window.onload = function() {
+	initializeCallbacks()
+};
 
+function initializeCallbacks(){
 	EloPeripheralManager.initialize("onPeripheralManagerReady")
 	EloHoneywellBarcodeManager.initialize("onHoneywellReady")
 	EloZebraBarcodeManager.initialize("onZebraReady")
@@ -10,38 +22,54 @@ window.onload = function() {
 	EloSocketMobileManager.initialize("onSocketReady")
 	EloStarPrinterManager.initialize("onStarPrinterReady")
 	EloStarScaleManager.initialize("onScaleReady")
-};
+}
 
 function onPeripheralManagerReady(){
         document.getElementById("PeripheralManagerCallbackText").innerText = "Peripheral Manager Initialized"
+	peripheralmanagerReady = true
 }
 
 function onHoneywellReady(){
 	 document.getElementById("HoneywellCallbackText").innerText = "Honeywell Scanner Initialized"
+	honeywellscannerReady = true
 }
 
 function onZebraReady(){
 	 document.getElementById("ZebraCallbackText").innerText = "Zebra Scanner Initialized"
+	 zebrascannerReady = true
 }
 
 function onHandheldReady(){
 	document.getElementById("HandheldCallbackText").innerText = "Handheld Scanner Initialized"
+	handheldscannerReady = true
 }
 
 function onEpsonReady(){
 	document.getElementById("EpsonPrinterCallbackText").innerText = "Epson Printer Initialized"
+	epsonprinterReady = true
 }
 
 function onSocketReady(){
 	document.getElementById("SocketMobileCallbackText").innerText = "Socket Mobile Scanner Initialized"
+	socketscannerReady = true
 }
 
 function onStarPrinterReady(){
 	document.getElementById("StarPrinterCallbackText").innerText = "Star Printer Initialized"
+	starprinterReady = true
 }
 
 function onScaleReady(){
         document.getElementById("StarScaleCallbackText").innerText= "Star Scale Initialized"
+	starscaleReady = true
+}
+
+function updateStatus(){
+	if (peripheralmanagerReady && honeywellscannerReady && zebrascannerReady && handheldscannerReady && epsonprinterReady
+	    && socketscannerReady && starprinterReady && starscaleReady){
+		
+	}
+	
 }
 
 
