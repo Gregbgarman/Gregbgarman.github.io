c9a6626aba0fe9799ae5a3874b6813a160b1edc6
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2023-08-15 16:42:21 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ window.onload = function() {
 	EloZebraBarcodeManager.initialize()
 	EloHandHeldBarcodeManager.initialize()
 	EloPeripheralManager.initialize()
-	EloStarPrinterManager.initialize()
+	EloStarPrinterManager.initialize("onStarPrinterReady")
 	EloStarScaleManager.initialize()
 
 };
