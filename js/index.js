0a00617d3471b95afa8367c33ad038a48cebbe95
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2023-08-15 16:37:46 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -55,8 +55,8 @@ function onEpsonReady(){
 }
 
 function onStarPrinterReady(){
-	//PrinterPortName = "BT:mC-Print3-star"
-	//printStarBarcode()
+	PrinterPortName = "BT:mC-Print3-star"
+	printStarBarcode()
 }
 
 function onHoneywellReady(){
