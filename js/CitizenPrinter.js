154fa340b8cfcad245e8359d3971e1384b43921c
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2024-01-18 15:39:18 -0500

    Update CitizenPrinter.js

--- js/CitizenPrinter.js
+++ js/CitizenPrinter.js
@@ -19,14 +19,22 @@ const CMP_TP_NORMAL = 12
 const CMP_ALIGNMENT_CENTER = 1
 const CMP_ALIGNMENT_LEFT = 0
 const CMP_FNT_DEFAULT = 0
+const CMP_FNT_UNDERLINE = 128
 const CMP_TXT_1WIDTH = 0
+const CMP_HRI_TEXT_BELOW = 2
 const CMP_CUT_PARTIAL_PREFEED = -4
 
 const CMP_QRCODE_EC_LEVEL_L = 48
+const CMP_BCS_Code39 = 109
 
 
 function searchBTCitizen(){
+    let CMP_PORT_Bluetooth_Insecure = 2
+    let searchTime = 10
+    let error = [1]
 
+    let bluetoothPrinters = EloCitizenPrinterManager.searchCitizenPrinter(CMP_PORT_Bluetooth_Insecure, searchTime, error)
+    document.getElementById("textField").value = bluetoothPrinters
 }
 
 function connectBTCitizen(){
@@ -34,8 +42,6 @@ function connectBTCitizen(){
 }
 
 function connectUSBCitizen(){
-            document.getElementById("textField").value = "btn click"
-
     let result = EloCitizenPrinterManager.connect(CMP_PORT_USB)
     if (result == CMP_SUCCESS){
         document.getElementById("CitizenPrinterAvailable").value = "printer ready"
@@ -56,9 +62,6 @@ function checkCitizenConnected(){
 }
 
 function printReceipt1Citizen(){
-
-
-    
     EloCitizenPrinterManager.transactionPrint(CMP_TP_TRANSACTION );     
     EloCitizenPrinterManager.printNormal(ESC + "|cA" + ESC + "|bC" + ESC + "|2C" + "The Pizza House" + ESC + "|lF" + ESC + "|lF");
     EloCitizenPrinterManager.printNormal(ESC + "|cA" + ESC + "|bC" + "123 Rainbow Road" + ESC + "|lF");
@@ -83,10 +86,36 @@ function printReceipt1Citizen(){
     if (result != CMP_SUCCESS){
         document.getElementById("textField").value = "transaction error"
     }
+    else{
+        document.getElementById("textField").value = "print success"
+    }
 }
 
 function printReceipt2Citizen(){
+    EloCitizenPrinterManager.transactionPrint( ESCPOSConst.CMP_TP_TRANSACTION );     //USB and BT both need this
+    EloCitizenPrinterManager.printText("Receipt\r\n\r\n\r\n", CMP_ALIGNMENT_CENTER, CMP_FNT_DEFAULT, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printText("TEL (123)-456-7890\r\n", CMP_ALIGNMENT_CENTER, CMP_FNT_DEFAULT, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printText("Thank you for coming to our shop\r\n\n", CMP_ALIGNMENT_CENTER, CMP_FNT_DEFAULT, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printText("Chicken               $10.00\r\n\n", CMP_ALIGNMENT_LEFT, CMP_FNT_DEFAULT, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printText("Hamburger             $20.00\r\n\n", CMP_ALIGNMENT_LEFT, CMP_FNT_DEFAULT, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printText("Pizza                 $30.00\r\n\n", CMP_ALIGNMENT_LEFT, CMP_FNT_DEFAULT, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printText("Lemons                $40.00\r\n\n", CMP_ALIGNMENT_LEFT, CMP_FNT_DEFAULT, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printText("Drink                 $50.00\r\n\r\n\n", CMP_ALIGNMENT_LEFT, CMP_FNT_DEFAULT, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printText("Excluded tax          $150.00\r\n\n", CMP_ALIGNMENT_LEFT, CMP_FNT_DEFAULT, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printText("Tax(5%)               $7.50\r\n\n", CMP_ALIGNMENT_LEFT, CMP_FNT_UNDERLINE, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printText("Total                 $157.50\r\n\r\n\n", CMP_ALIGNMENT_LEFT, CMP_FNT_DEFAULT, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printText("Payment               $200.00\r\n\n", CMP_ALIGNMENT_LEFT, CMP_FNT_DEFAULT, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printText("Change                $42.50\r\n\r\n", CMP_ALIGNMENT_LEFT, CMP_FNT_DEFAULT, CMP_TXT_1WIDTH);
+    EloCitizenPrinterManager.printBarCode("1234567890", CMP_BCS_Code39, 40, 2, CMP_ALIGNMENT_CENTER, CMP_HRI_TEXT_BELOW);
+    EloCitizenPrinterManager.cutPaper(CMP_CUT_PARTIAL_PREFEED );
 
+    let result = EloCitizenPrinterManager.transactionPrint(CMP_TP_NORMAL);
+    if (result != CMP_SUCCESS){
+        document.getElementById("textField").value = "transaction error"
+    }
+    else{
+        document.getElementById("textField").value = "print success"
+    }
 }
 
 function printImageCitizen(){
@@ -101,6 +130,4 @@ function disconnectCitizen(){
     else{
         document.getElementById("textField").value = "error disconnecting"
     }
-     
 }
-
