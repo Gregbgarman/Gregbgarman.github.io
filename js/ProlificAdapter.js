4ee53ac0c0064b78a853a82ba3a7ef9eb1baf3ad
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2024-03-04 15:07:51 -0500

    Create ProlificAdapter.js

---
+++ js/ProlificAdapter.js
@@ -0,0 +1,8 @@
+document.getElementById("prolificOpen").addEventListener("click", prolificOpen)
+
+function prolificOpen(){
+    let array = new Uint8Array(1);
+  
+    document.getElementById("textField").value = EloProlificAdapterManager.write(array)
+     
+}
