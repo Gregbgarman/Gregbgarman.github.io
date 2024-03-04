52fd3ea524ac9b53d13a9c8de0507ef4df6b7e40
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2024-03-04 15:44:04 -0500

    Update ProlificAdapter.js

--- js/ProlificAdapter.js
+++ js/ProlificAdapter.js
@@ -1,5 +1,16 @@
 document.getElementById("prolificOpen").addEventListener("click", prolificOpen)
 
+
+ EloProlificAdapterManager.initialize("mycallbackprolific")
+
+function mycallbackprolific(isbound){
+    if (isbound == "true"){
+        document.getElementById("textField").value = "service bound adapter"
+    }
+
+    
+}
+
 function prolificOpen(){
     let array = new Uint8Array(1);
   
