f078b19d372e4c4c1177b66d857d4fcf7ccc7bc9
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2024-03-04 16:45:18 -0500

    Update ProlificAdapter.js

--- js/ProlificAdapter.js
+++ js/ProlificAdapter.js
@@ -6,6 +6,15 @@ document.getElementById("prolificOpen").addEventListener("click", prolificOpen)
 function mycallbackprolific(isbound){
     if (isbound == "true"){
         document.getElementById("textField").value = "service bound adapter"
+
+        if (!EloProlificAdapterManager.PL2303USBFeatureSupported()) {
+             document.getElementById("textField").value = "usb feature not supported"
+             return
+        }
+
+        EloProlificAdapterManager.enumerate()
+
+     
     }
 
     
@@ -14,7 +23,7 @@ function mycallbackprolific(isbound){
 function prolificOpen(){
     let array = new Uint8Array([1,2,3,4,5]);
   
-    document.getElementById("textField").value = EloProlificAdapterManager.write(array,5)
+    document.getElementById("textField").value = EloProlificAdapterManager.write(array)
 
     
 }
