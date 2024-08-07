e0f4212d3150bc06d91c568d902df765be9542f3
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2024-08-07 10:08:17 -0400

    Update pr1000.js

--- js/pr1000.js
+++ js/pr1000.js
@@ -1,6 +1,11 @@
 document.getElementById("PR1000_connect").addEventListener("click", connectPR1000)
 document.getElementById("PR1000_getUSBPrinters").addEventListener("click", getPR1000UsbPrinters)
 
+document.getElementById("PR1000_barcode_test").addEventListener("click", barcodeTestPR1000)
+
+
+
+
 
 function connectPR1000(){
     EloPR1000PrinterManager.connect("sdfds")
@@ -8,4 +13,8 @@ function connectPR1000(){
 
 function getPR1000UsbPrinters(){
     EloPR1000PrinterManager.getAvailableUsbPrinters()
+}
+
+function barcodeTestPR1000(){
+    EloPR1000BarcodeManager.test()
 }
