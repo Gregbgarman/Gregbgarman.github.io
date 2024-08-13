3dbdde27654fe4b52a8bf7234ebbcb2fa0e8443e
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2024-08-13 13:55:46 -0400

    Update pr1000.js

--- js/pr1000.js
+++ js/pr1000.js
@@ -2,37 +2,43 @@ document.getElementById("PR1000_connect").addEventListener("click", connectPR100
 document.getElementById("PR1000_getUSBPrinters").addEventListener("click", getPR1000UsbPrinters)
 
 document.getElementById("PR1000_barcode_test").addEventListener("click", barcodeTestPR1000)
+document.getElementById("PR1000_beep_test").addEventListener("click", beepTestPR1000)
 
 
-let selectedPrinter = ""
+
+
 
 
 function connectPR1000(){
-    EloPR1000PrinterManager.connect("sdfds")
+    let printer = document.getElementById("textField").value
+    EloPR1000PrinterManager.connect(printer)
 }
 
 function getPR1000UsbPrinters(){
     
     let printerArray = JSON.parse(EloPR1000PrinterManager.getAvailableUsbPrinters())
-    console.log("array is " + printerArray);
+    let deviceName = "no devices found";
     
     for (let i = 0; i < printerArray.length; i++){
-       // let printerObj = JSON.parse(printerArray[i])
         let printerObj = printerArray[i]
-        console.log("object is " + printerObj);
-
-        let deviceName = printerObj["deviceName"]
-       // let vendorId = printerObj.vendorId
-       // let productId = printerObj.productId
+        deviceName = printerObj["deviceName"]
+        let vendorId = printerObj.vendorId
+        let productId = printerObj.productId
         console.log("devicename is " + deviceName);
-       // console.log("vendorId is " + vendorId);
-       // console.log("productId is " + productId);
-        
+        console.log("vendorId is " + vendorId);
+        console.log("productId is " + productId);        
     }
-    console.log("the end" );
+    document.getElementById("textField").value = deviceName
     
+}
+
+function beepTestPR1000(){
+    EloPR1000EscCmdManager.append(EloPR1000EscCmdManager.getBeepCmd())
+    EloPR1000EscCmdManager.writeMsgAsync(EloPR1000EscCmdManager.getAppendCmds())
 }
 
 function barcodeTestPR1000(){
     EloPR1000BarcodeManager.test()
 }
+
+
