999bf22708bcabf418e4af39711c88412c330ab2
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2024-08-16 20:21:32 -0400

    Update pr1000.js

--- js/pr1000.js
+++ js/pr1000.js
@@ -53,7 +53,7 @@ pr1000EscCmd.append(pr1000EscCmd.getBeepCmd());
 }
 
 function barcodeTestPR1000(){
-        EloPR1000EscCmdManager.gregTest()
+        //EloPR1000EscCmdManager.gregTest()
 
     
     console.log(typeof EloPR1000EscCmdManager);
