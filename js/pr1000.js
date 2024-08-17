f4ede7084906f6357771b0a9132f15c09dd68182
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2024-08-17 12:50:51 -0400

    Update pr1000.js

--- js/pr1000.js
+++ js/pr1000.js
@@ -1,21 +1,16 @@
 document.getElementById("PR1000_connect").addEventListener("click", connectPR1000)
 document.getElementById("PR1000_getUSBPrinters").addEventListener("click", getPR1000UsbPrinters)
-
 document.getElementById("PR1000_barcode_test").addEventListener("click", barcodeTestPR1000)
 document.getElementById("PR1000_beep_test").addEventListener("click", beepTestPR1000)
-
 document.getElementById("PR1000_image_test").addEventListener("click", imageTestPR1000)
-
-
-
-
-
-
-function connectPR1000(){
-    let printer = document.getElementById("textField").value
-    EloPR1000PrinterManager.connect(printer)
-}
-
+document.getElementById("PR1000_getWifiPrinters").addEventListener("click", getPR1000WifiPrinters)
+document.getElementById("PR1000_text_test").addEventListener("click",textTestPR1000)
+document.getElementById("PR1000_status_test").addEventListener("click",getStatusPR1000)
+document.getElementById("PR1000_drawer_test").addEventListener("click",drawerTestPR1000)
+document.getElementById("PR1000_disconnect").addEventListener("click",disconnectPR1000)
+    
+       
+        
 function getPR1000UsbPrinters(){
     
     let printerArray = JSON.parse(EloPR1000PrinterManager.getAvailableUsbPrinters())
@@ -33,6 +28,30 @@ function getPR1000UsbPrinters(){
     }
     document.getElementById("textField").value = deviceName
     
+}
+
+
+function getPR1000WifiPrinters(){
+
+}
+
+
+function connectPR1000(){
+    let printer = document.getElementById("textField").value
+    EloPR1000PrinterManager.connect(printer)
+}
+
+function getStatusPR1000(){
+
+}
+
+function textTestPR1000(){
+
+}
+
+function drawerTestPR1000(){
+     EloPR1000EscCmdManager.addOpenMoneyBoxCmd()
+     EloPR1000PrinterManager.writeMsgAsync(EloPR1000EscCmdManager)
 }
 
 function beepTestPR1000(){
@@ -59,6 +78,10 @@ function barcodeTestPR1000(){
 }
 
 function imageTestPR1000(){
+
+}
+
+function disconnectPR1000(){
 
 }
 
