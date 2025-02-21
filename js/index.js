bf3256392d435f180bc369d78c0b0ff5f09909af
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2025-02-21 18:17:50 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ function setOnReadyCallbacks(){
 
 
   try{
-        EloHoneywellBarcodeManager.initialize("onHoneywellReady")
+       // EloHoneywellBarcodeManager.initialize("onHoneywellReady")
     }catch(error){
        /*
          Make sure Webview Hardware Access toggle is enabled
