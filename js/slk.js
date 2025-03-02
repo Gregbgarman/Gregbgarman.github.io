cae8bf70ffd1256fddf85a50d5ffaa3005577a9d
Gregbgarman <75956079+Gregbgarman@users.noreply.github.com>
2025-03-02 16:36:00 -0500

    Update slk.js

--- js/slk.js
+++ js/slk.js
@@ -12,46 +12,49 @@ document.getElementById("slkrightflashing").addEventListener("click",slkrightfla
 document.getElementById("slkrightoff").addEventListener("click", slkrightoff)
 
 
+let port_top = 5
+let port_right = 4
+
 function slktopred(){
     document.getElementById("textField").value = "button clicked"
-    EloPeripheralManager.setLightByPort(4 , 0 , true)
+    EloPeripheralManager.setLightByPort(port_top , 0 , true)
 }
 
 function slktopblue(){
-EloPeripheralManager.setLightByPort(4 , 8 , true)
+EloPeripheralManager.setLightByPort(port_top , 8 , true)
 }
 
 function slktopgreen(){
-EloPeripheralManager.setLightByPort(4 , 7 , true)
+EloPeripheralManager.setLightByPort(port_top , 7 , true)
 }
 
 function slktopflashing(){
-EloPeripheralManager.setFlashingByPort(4, 0, 250, true)
+EloPeripheralManager.setFlashingByPort(port_top, 0, 250, true)
 }
 
 function slktopoff(){
-EloPeripheralManager.setLightByPort(4 , 0 , false)
+EloPeripheralManager.setLightByPort(port_top , 0 , false)
 }
 
 
 function slkrightred(){
-EloPeripheralManager.setLightByPort(5 , 0 , true)
+EloPeripheralManager.setLightByPort(port_right , 0 , true)
 }
 
 function slkrightblue(){
-EloPeripheralManager.setLightByPort(5 , 8 , true)
+EloPeripheralManager.setLightByPort(port_right , 8 , true)
 }
 
 function slkrightgreen(){
-EloPeripheralManager.setLightByPort(5 , 7 , true)
+EloPeripheralManager.setLightByPort(port_right , 7 , true)
 }
 
 function slkrightflashing(){
-EloPeripheralManager.setFlashingByPort(5, 0, 250, true)
+EloPeripheralManager.setFlashingByPort(port_right, 0, 250, true)
 }
 
 function slkrightoff(){
-EloPeripheralManager.setLightByPort(5 , 0 , false)
+EloPeripheralManager.setLightByPort(port_right, 0 , false)
 }
 
     
